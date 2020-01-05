981d0dd168de73e20ed3bb2b3758918e840324e5
ostefani <ostefanishina@gmail.com>
2020-01-05 20:17:45 +0300

    Add theme provider to the app

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-const Button = styled.attrs({ className: 'button' })`
+const Button = styled.div.attrs({ className: 'button' })`
     min-width: 70px;
+    background-color: ${({ theme: { secondary: { base } } }) => base};
 `;
 
 export { Button };
