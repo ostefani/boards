9196c326119f268648b559413078cbde5254ed56
ostefani <ostefanishina@gmail.com>
2020-01-07 21:45:38 +0300

    Add fullWidthButton story

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -14,5 +14,12 @@ const defaultProps = {
     type: 'button',
     name: 'Primary',
 };
+const fullWidthProps = {
+    type: 'button',
+    name: 'Full Width Button',
+    isFullWidth: true,
+};
 
-export const DefaultButton = () => <Button {...actionsData} {...defaultProps}>Hello Button</Button>;
+export const DefaultButton = () => <Button {...actionsData} {...defaultProps} />;
+
+export const FullWidthtButton = () => <Button {...actionsData} {...fullWidthProps} />;
