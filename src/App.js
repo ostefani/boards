3fef1dc2bf0c41081dd533b1fff2cae9947252aa
Olga <59526402+ostefani@users.noreply.github.com>
2020-01-08 09:47:30 +0300

    Revert "Button"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
 import Button from 'src/components/Button';
 import Input from 'src/components/Input';
-import theme from 'src/settings/theme';
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <div className="root">
-                <h1>Boards</h1>
-                <Button />
-            </div>
-        </ThemeProvider>
+        <div className="root">
+            <h1>Boards</h1>
+            <Button />
+        </div>
     );
 }
 
