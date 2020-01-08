bd6c21c441cb81c44dd9cd9543427ac41bfefaf3
Olga <59526402+ostefani@users.noreply.github.com>
2020-01-08 09:57:11 +0300

    Revert "Revert "Button""

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,63 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+    Button, Ripple, RippleContainer, Text,
+} from './style';
 
-export default () => <button type="button" onClick={() => 'hello button'}>Hello button</button>;
+export default ({
+    type, isFullWidth, isDisabled, name, onClick,
+}) => {
+    const button = useRef(null);
+    const [buttonParams, setButtonParams] = useState({});
+    const [isClicked, setIsClicked] = useState(false);
+    const [ripples, setRipples] = useState([]);
+
+    const handleAnimationEnd = (e, id) => {
+        setRipples([...ripples.filter(ripple => (ripple.id !== id))]);
+    };
+
+    const handleClick = e => {
+        setIsClicked(true);
+        e.stopPropagation();
+        onClick();
+        setRipples([...ripples, {
+            posX: e.pageX - e.currentTarget.offsetLeft,
+            posY: e.pageY - e.currentTarget.offsetTop,
+            id: Math.random().toString(),
+            width: buttonParams.width,
+            height: buttonParams.height,
+        }]);
+    };
+
+    useEffect(() => {
+        setButtonParams(button.current.getBoundingClientRect());
+    }, []);
+
+    useEffect(() => {
+        if (ripples.length === 0) {
+            setIsClicked(false);
+        }
+    }, [ripples]);
+
+    return (
+        <Button
+            ref={button}
+            type={type}
+            disabled={isDisabled}
+            isFullWidth={isFullWidth}
+            onClick={handleClick}
+        >
+            <Text>{name}</Text>
+            {isClicked && (
+                <RippleContainer>
+                    {ripples.map(ripple => (
+                        <Ripple
+                            key={ripple.id}
+                            ripple={ripple}
+                            onAnimationEnd={e => handleAnimationEnd(e, ripple.id)}
+                        />
+                    ))}
+                </RippleContainer>
+            )}
+        </Button>
+    );
+};
