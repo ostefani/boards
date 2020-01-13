60f1b9e19ebc6116783181b714376432bd373064
ostefani <ostefani@protonmail.com>
2020-01-13 18:56:52 +0300

    Fix styles for input

--- src/components/Logo/style.js
+++ src/components/Logo/style.js
@@ -5,6 +5,7 @@ export default styled(Link)`
         font-family: ${({ theme: { font: { lato } } }) => lato};
         text-transform: uppercase;
         color: ${({ theme: { secondary: { base } } }) => base};
+        font-size: 28px;
 
         &:hover {
             color: ${({ theme: { secondary: { dark } } }) => dark};
