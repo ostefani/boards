2f48fb8bec0c7d431bd871cc88b61a31864200fa
ostefani <ostefani@protonmail.com>
2020-01-14 18:09:41 +0300

    Add structure for jumbotron

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Container,
+    Title,
+    MainTitle,
+    Subtitle,
+} from './style';
+
+export default () => {
+    return (
+        <Container>
+            <Title>
+                <MainTitle>Boards</MainTitle>
+                <Subtitle>All you need</Subtitle>
+                <Subtitle>to manage your project</Subtitle>
+            </Title>
+        </Container>
+    )
+}
---
+++ src/components/Jumbotron/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    position: relative;
+`;
+const Title = styled.h1`
+
+`;
+const MainTitle = styled.span``;
+const Subtitle = styled.span``;
+
+export {
+    Container,
+    Title,
+    MainTitle,
+    Subtitle,
+};
