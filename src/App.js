ea86af045362bbf2428be7602d2908daeb5c7a3d
ostefani <ostefani@protonmail.com>
2020-01-16 20:30:59 +0300

    Add main routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'src/settings/theme';
 import { BrowserRouter } from 'react-router-dom';
+import MainRoute from './router';
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <BrowserRouter>
-
+                <MainRoute />
             </BrowserRouter>
         </ThemeProvider>
     );
--- src/router.js
+++ src/router.js
@@ -2,11 +2,36 @@ import React from 'react';
 import {
     Route, Redirect, Switch, withRouter,
 } from 'react-router-dom';
+import Home from 'src/containers/Home';
+import Auth from 'src/containers/Auth';
+import Boards from 'src/containers/Boards';
+console.log('Auth: ', Auth);
+
+const ProtectedRoute = ({ children, ...rest }) => {
+    const isAuthenticated = false;
+    return (
+        <>
+            {isAuthenticated
+                ? (<Route {...rest}>{children}</Route>)
+                : (<Redirect to="/auth" />)}
+        </>
+    );
+};
 
 export default () => {
     return (
         <Switch>
-            <Route path="/" />
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/auth/">
+                <Auth />
+            </Route>
+            <Route path="/boards">
+                <ProtectedRoute>
+                    <Boards />
+                </ProtectedRoute>
+            </Route>
         </Switch>
     );
 };
