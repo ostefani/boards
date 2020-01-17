ccc10dba07ff09a502524fe8bd8d6c8e68e32ee6
ostefani <ostefani@protonmail.com>
2020-01-17 20:16:02 +0300

    Fix logo

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import WithStyle from './style';
 
-export default WithStyle(({ to, children, className }) => <Link to={to} className={className}>{children}</Link>);
+export default WithStyle(({ to, children, className }) => (
+    <Link to={to} className={className}>{children}</Link>
+));
