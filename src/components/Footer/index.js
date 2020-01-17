8ebf0c02c750ab7bd79b7eda95608a561b2bdc4b
ostefani <ostefani@protonmail.com>
2020-01-17 20:12:32 +0300

    Add link to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {
-    Footer,
-    Link,
-} from './style';
+import WithStyle from 'src/components/Link/style';
+import Footer from './style';
+
+const Link = WithStyle(({ href, className }) => (
+    <a href={href} className={className} target="_blank" rel="noopener noreferrer">Olga Stefanishyna</a>
+));
 
 export default () => (
     <Footer>
-        <Link to="https://github.com/ostefani" className="footer-link">Olga Stefanishyna</Link>
+        <Link href="https://github.com/ostefani" className="footer-link" />
     </Footer>
 );
