e46707de84671bc33e958cfa4fed083e368220ad
ostefani <ostefani@protonmail.com>
2020-02-02 13:06:57 +0300

    Refactor login function

--- server/graphql/resolvers/login/index.js
+++ server/graphql/resolvers/login/index.js
@@ -7,46 +7,17 @@ dotenv.config();
 
 const secret = process.env.SECRET;
 
-const checkPassword = async (password, hash) => {
-    try {
-        const isMatch = await bcrypt.compare(password, hash);
-        return isMatch;
-    }
-    catch (e) {
-        return e;
-    }
-};
-
-const getToken = async id => {
-    try {
-        const token = await jwt.sign(id, secret);
-        if (token) {
-            return token;
-        }
-        throw new Error('Some error occurred, try again later');
-    }
-    catch (e) {
-        throw new Error(e.message);
-    }
-};
-
-export default async function login({ email, password }) {
+export default async ({ email, password }) => {
     try {
         const user = await User.findOne({ email });
-        if (!user) {
-            throw new Error('Email does not exist!');
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = await jwt.sign({ id: user._id }, secret);
+            return { token, password: null, ...user._doc };
         }
-        return checkPassword(password, user.password)
-            .then(isMatch => {
-                if (isMatch) {
-                    return getToken({ id: user._id })
-                        .then(token => ({ token, password: null, ...user._doc }));
-                }
-                throw new Error('Password is incorrect');
-            })
-            .catch(e => e);
+        throw new Error('Password is incorrect');
     }
     catch (e) {
         return e;
     }
-}
+};
