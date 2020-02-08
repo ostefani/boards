52dcd39166a21e6d733159a8ff40fa67b508e81c
ostefani <ostefani@protonmail.com>
2020-02-08 10:42:58 +0300

    Refactor authentication

--- src/authentication.js
+++ src/authentication.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import authActions from 'src/redux/user/actions';
 import { verifyToken } from 'src/services/auth';
+
 const { subscribeUser, unsubscribeUser } = authActions;
 
-
-export default (Component) => {
+export default Component => (({
+    path, children, location, computedMatch,
+}) => {
     const dispatch = useDispatch();
     const isAuthenticated = useSelector(state => state.user.isAuthenticated);
     verifyToken().then(data => {
@@ -22,7 +24,13 @@ export default (Component) => {
             console.log('e: ', error);
             dispatch(unsubscribeUser());
         });
-    return function(props) {
-        return <Component isAuthenticated={isAuthenticated} {...props} />;
-    }
-};
+    return (
+        <Component
+            isAuthenticated={isAuthenticated}
+            path={path}
+            location={location}
+            computedMatch={computedMatch}
+        >{children}
+        </Component>
+    );
+});
--- src/router.js
+++ src/router.js
@@ -2,87 +2,57 @@ import React from 'react';
 import {
     Route, Redirect, Switch,
 } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
 import Home from 'src/containers/Home';
 import Login from 'src/containers/Auth/Login';
 import SignUp from 'src/containers/Auth/SignUp';
 import Boards from 'src/containers/Boards';
 import Profile from 'src/containers/Profile';
-import { verifyToken } from 'src/services/auth';
-import authActions from 'src/redux/user/actions';
-
-const { subscribeUser, unsubscribeUser } = authActions;
+import WithReduxAuth from './authentication';
 
 
-const ProtectedRoute = ({ children, ...rest }) => {
-    const dispatch = useDispatch();
-    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
-    verifyToken().then(data => {
-        const { errors } = data;
-        if (errors) {
-            console.log('error: ', errors[0].message);
-            dispatch(unsubscribeUser());
-        }
-        else {
-            dispatch(subscribeUser());
-        }
-    })
-        .catch(error => {
-            console.log('e: ', error);
-            dispatch(unsubscribeUser());
-        });
-    return (
-        <>
-            {isAuthenticated
-                ? (<Route {...rest}>{children}</Route>)
-                : (<Redirect to="/login" />)}
-        </>
-    );
-};
-const AuthRoute = ({ children, ...rest }) => {
-    const dispatch = useDispatch();
-    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
-    verifyToken().then(data => {
-        const { errors } = data;
-        if (errors) {
-            console.log('error: ', errors[0].message);
-            dispatch(unsubscribeUser());
-        }
-        else {
-            dispatch(subscribeUser());
-        }
-    })
-        .catch(error => {
-            console.log('e: ', error);
-            dispatch(unsubscribeUser());
-        });
-    return (
-        <>
-            {isAuthenticated
-                ? (<Redirect to="/boards" />)
-                : (<Route {...rest}>{children}</Route>)}
-        </>
-    );
-};
+const ProtectedRoute = WithReduxAuth(({
+    children, isAuthenticated, path, location, computedMatch,
+}) => (
+    <>
+        {isAuthenticated
+            ? (
+                <Route path={path} location={location} computedMatch={computedMatch}>
+                    {children}
+                </Route>
+            )
+            : (<Redirect to="/login" />)}
+    </>
+));
+const AuthRoute = WithReduxAuth(({
+    children, isAuthenticated, path, location, computedMatch,
+}) => (
+    <>
+        {isAuthenticated
+            ? (<Redirect to="/boards" />)
+            : (
+                <Route path={path} location={location} computedMatch={computedMatch}>
+                    {children}
+                </Route>
+            )}
+    </>
+));
 
-export default () => {
-    return (
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <AuthRoute path="/login">
-                <Login />
-            </AuthRoute>
-            <AuthRoute path="/signup">
-                <SignUp />
-            </AuthRoute>
-            <ProtectedRoute path="/boards">
-                <Boards />
-            </ProtectedRoute>
-            <ProtectedRoute path="/profile">
-                <Profile />
-            </ProtectedRoute>
-        </Switch>
-    );
-};
+export default () => (
+    <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+        <AuthRoute path="/login">
+            <Login />
+        </AuthRoute>
+        <AuthRoute path="/signup">
+            <SignUp />
+        </AuthRoute>
+        <ProtectedRoute path="/boards">
+            <Boards />
+        </ProtectedRoute>
+        <ProtectedRoute path="/profile">
+            <Profile />
+        </ProtectedRoute>
+    </Switch>
+);
