3995f21279b237528dd4feb9aa134c85f6e53f22
ostefani <ostefani@protonmail.com>
2020-02-15 13:47:24 +0300

    Validate form in the signup page

---
+++ src/containers/Auth/SignUp/schemas.js
@@ -0,0 +1,24 @@
+export const stateSchema = {
+    username: { value: '', error: '', isValidated: false },
+    email: { value: '', error: '', isValidated: false },
+    password: { value: '', error: '', isValidated: false },
+};
+export const stateValidatorSchema = {
+    username: {
+        required: true,
+        validator: {
+            regEx: /^[a-zA-Z]\w{2,7}$/,
+            error: 'Username must be 3-8 alphanumeric characters and begin with a letter',
+        },
+    },
+    email: {
+        required: true,
+        validator: {
+            regEx: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            error: 'Invalid email format.',
+        },
+    },
+    password: {
+        required: true,
+    },
+};
