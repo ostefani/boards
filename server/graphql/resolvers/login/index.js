d85781bebf1d3d803aebb030487e1145f1d23261
ostefani <ostefani@protonmail.com>
2020-02-22 16:43:05 +0300

    Fix code style

--- server/graphql/resolvers/login/index.js
+++ server/graphql/resolvers/login/index.js
@@ -2,7 +2,7 @@ import getUser from '../../../services/getUser';
 import { LoginShema } from '../../../schemas';
 
 export default async ({ email, password }) => {
-    const { error, value } = LoginShema.validate({ email, password });
+    const { error, _ } = LoginShema.validate({ email, password });
     if (!error) {
         return getUser({ email, password });
     }
--- server/graphql/resolvers/signup/index.js
+++ server/graphql/resolvers/signup/index.js
@@ -2,7 +2,7 @@ import postUser from '../../../services/postUser';
 import { SignupShema } from '../../../schemas';
 
 export default ({ userInput: { username, email, password } }) => {
-    const { error, value } = SignupShema.validate({ username, email, password });
+    const { error, _ } = SignupShema.validate({ username, email, password });
     if (!error) {
         return postUser({ username, email, password });
     }
