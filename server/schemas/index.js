59a9dcf86721da6b5b76c3d148d43202536d3d60
ostefani <ostefani@protonmail.com>
2020-02-22 16:23:43 +0300

    Fix code style

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -18,7 +18,7 @@ export const LoginShema = Joi.object().keys({
         .string()
         .trim()
         .required()
-        .pattern(/^[a-zA-Z0-9]{3,16}$/)
+        .pattern(/^[a-zA-Z0-9]{8,16}$/)
         .error(new CustomError('Invalid password format', 'password', 'AuthUserError'))
         .options({
             skipFunctions: true,
@@ -29,13 +29,12 @@ export const SignupShema = Joi.object().keys({
     username: Joi
         .string()
         .required()
-        .pattern(new RegExp('/^[a-zA-Z]\w{2,7}$/')),
+        .pattern(/^[a-zA-Z]\w{2,7}$/),
 
     email: Joi
         .string()
         .trim()
         .required()
-        // .email({ minDomainSegments: 1, tlds: { allow: ['com', 'net'] } })
         .pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/)
         .error(new CustomError('Invalid email format.', 'email', 'AuthUserError'))
         .options({
@@ -47,7 +46,7 @@ export const SignupShema = Joi.object().keys({
         .string()
         .trim()
         .required()
-        .pattern(/^[a-zA-Z0-9]{3,16}$/)
+        .pattern(/^[a-zA-Z0-9]{8,16}$/)
         .error(new CustomError('Invalid password format', 'password', 'AuthUserError'))
         .options({
             skipFunctions: true,
