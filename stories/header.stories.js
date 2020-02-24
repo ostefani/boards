bcaf6df23cf1220f67a15f6b8bb9cf2492db2c9a
ostefani <ostefani@protonmail.com>
2020-02-24 09:33:33 +0300

    Fix code style

--- stories/header.stories.js
+++ stories/header.stories.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { HeaderComponent } from 'src/components/Header';
-import Dropdown from 'src/components/Dropdown';
-import { verify } from 'src/redux/user/actions';
-
 
 export default {
     title: 'Header',
     component: HeaderComponent,
     decorators: [storyFn => <div style={{ width: '100%' }}>{storyFn()}</div>],
 };
+
 const actionsData = {
     onClick: action('clicked'),
 };
-//'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTUxMzY5OTUwYTA2MGFlODQ0NGZlOTkiLCJ1c2VybmFtZSI6InRlc3QiLCJlbWFpbCI6InRlc3RAdC50dCIsInBhc3N3b3JkIjpudWxsLCJfX3YiOjAsImlhdCI6MTU4MjUyMTU1Nn0.IFv-MijckSl4itS2yiHmQLvLrio0yCRHmFNMwTSaNEM'
+
 const defaultProps = {
     user: {
         username: 'ostefani',
     },
 };
+
 const authProps = {
     username: 'ostefani',
     isAuthenticated: true,
