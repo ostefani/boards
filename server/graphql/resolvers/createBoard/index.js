78f7582e6dcb5ab97d5daf27a1214bb495a6e06a
ostefani <ostefani@protonmail.com>
2020-03-09 19:33:31 +0300

    Save user id to board schema

--- server/graphql/resolvers/createBoard/index.js
+++ server/graphql/resolvers/createBoard/index.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import User from '../../../models/user';
 import Board from '../../../models/board';
 import CustomError from '../../../customError';
 
@@ -11,11 +12,15 @@ export default async ({ boardInput: { title } }, context) => {
     try {
         const board = new Board({
             title,
+            createdBy: context.user._id,
         });
-        console.log('board: ', board._doc);
         const result = await board.save();
         if (result) {
-            return { ...board._doc };
+            console.log('result: ', result._doc);
+            // const user = await User.findOne({ _id: context.user._id });
+            //const user = await User.updateOne({ _id: context.user._id }, { $set: {  } })
+            //console.log('user: ', user);
+            return { ...result._doc };
         }
         throw new CustomError('Title has incorrect format', 'password', 'AuthUserError');
     }
