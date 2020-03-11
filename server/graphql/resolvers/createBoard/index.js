e39ad52467b005607df604d89f47949a4aa47e1d
ostefani <ostefani@protonmail.com>
2020-03-11 19:02:36 +0300

    Check authentication before creting a board

--- server/graphql/resolvers/createBoard/index.js
+++ server/graphql/resolvers/createBoard/index.js
@@ -1,28 +1,20 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import User from '../../../models/user';
 import Board from '../../../models/board';
 import CustomError from '../../../customError';
 
-const secret = process.env.SECRET;
-
 export default async ({ boardInput: { title } }, context) => {
-    console.log('context: ', context.user);
-    console.log('boardTitle: ', title);
     try {
+        if (!context.user) {
+            throw new CustomError('You are not authenticated', 'verification', 'AuthUserError');
+        }
         const board = new Board({
             title,
             createdBy: context.user._id,
         });
         const result = await board.save();
         if (result) {
-            console.log('result: ', result._doc);
-            // const user = await User.findOne({ _id: context.user._id });
-            //const user = await User.updateOne({ _id: context.user._id }, { $set: {  } })
-            //console.log('user: ', user);
             return { ...result._doc };
         }
-        throw new CustomError('Title has incorrect format', 'password', 'AuthUserError');
     }
     catch (e) {
         return e;
