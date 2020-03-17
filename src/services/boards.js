a1cfd6aaa36af127071380b7bdb80dcb96c68890
ostefani <ostefani@protonmail.com>
2020-03-17 19:02:35 +0300

    Add getBoard service

--- src/services/boards.js
+++ src/services/boards.js
@@ -50,3 +50,29 @@ export const getBoards = () => {
         })
         .catch(e => ({ errors: [{ message: e.message }] }));
 };
+
+export const getBoard = () => {
+    const token = localStorage.getItem('boards') || '';
+    const URI = `${API}/api`;
+    const body = `query {
+        getBoardData {
+            _id, title, tasks
+        }
+    }`;
+    const query = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ query: body }),
+    };
+    return fetch(URI, query)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw Error('We run into proplem. Try again later.');
+        })
+        .catch(e => ({ errors: [{ message: e.message }] }));
+};
